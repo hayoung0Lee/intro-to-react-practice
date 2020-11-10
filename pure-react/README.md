8abdd7b5856d58dfd12e080737465f1b465a0bb9
hayoung0Lee <hayoung0.lee@gmail.com>
2020-11-10 23:52:54 +0900

    Pure React End

--- pure-react/README.md
+++ pure-react/README.md
@@ -7,3 +7,11 @@
 2. Dom
    - https://wit.nts-corp.com/2019/02/14/5522
    - https://developer.mozilla.org/ko/docs/Web/API/Document_Object_Model/%EC%86%8C%EA%B0%9C
+
+   - 돔? https://www.youtube.com/watch?v=H63dVFDuJDM&ab_channel=TheNetNinja
+
+
+  - virtual dom: react가 쓰는것, createElement해서 만들어낸 div, 근데 얘는 돔은 아니고 객체다. (React.div 뭐이런거)
+    - https://stackoverflow.com/questions/21965738/what-is-virtual-dom
+
+   - React dom: https://reactjs.org/docs/react-dom.html 은 진짜 이걸 그린다. dom으로 뭐 비교해서 변한거만 그리는 그런 로직 여기있다. 
--- pure-react/src/App.js
+++ pure-react/src/App.js
@@ -1,18 +1,30 @@
-const Pet = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Marty'),
-    React.createElement('h1', {}, 'Dakota'),
-    React.createElement('h1', {}, 'Marty and Dakota'),
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h1", {}, animal),
+    React.createElement("h1", {}, breed),
   ]);
 };
 
 const App = () => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
